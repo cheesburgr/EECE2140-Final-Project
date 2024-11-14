68b0103e428044a167c53df25cbd5b35fb44e8aa
cheesburgr <lmk51364@gmail.com>
2024-11-14 11:21:38 -0500

    Add files via upload

--- wordPicker.py
+++ wordPicker.py
@@ -1,47 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 28 11:56:32 2024
+Created on Thu Nov 14 11:13:58 2024
 
 @author: lmk51
 """
 
 import random
 
-class wordPicker:
-    animals = ['Dog',
-               'Cat', 
-               'Iguana',
-               'Elephant',
-               '',
-               '',
-               '',
-               '',
-               '',
-               '',
-               '',
-               '',
-               ]
-    food = ['', 
-            '', 
-            '']
-    countries = ['', 
-            '', 
-            '']
-    
-    def __init__(self, category):
-        self.category = category
-        if category.lower() == "animals":
-            self.category = self.animals
-        if category.lower() == 'food':
-            self.category = self.food
-        if category.lower() == 'countries':
-            self.category = self.countries
-        if category.lower() == 'all':
-            self.category = self.countries + self.animals + self.food
-    
-    @classmethod
-    def printBank():
-        print(self.wordBank)
-        
-        
-choose = wordPicker()
+class WordBank:
+    def __init__(self, words):
+        """Initialize the WordBank with a list of words."""
+        self.words = words
+
+    def add_word(self, word):
+        """Add a new word to the word bank."""
+        self.words.append(word)
+
+    def get_random_word(self):
+        """Select and return a random word from the word bank."""
+        if not self.words:
+            return None  # Return None if the word bank is empty
+        return random.choice(self.words)
+
+# Example usage
+
+fruits = WordBank(["apple", "banana", "cherry", "date", "elderberry"])
+places = WordBank(["australia", "california", ""])
+
+    # Get a random word
+random_word = places.get_random_word()
+print(f"Randomly selected word: {random_word}")
